1d32eb161b417087f915a26cc026fe1dde403992
Nishit <nishitdjoshi@gmail.com>
2021-08-26 21:40:04 -0500

    Vue props to pass data between components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="todos">
+      <Todos v-bind:todos="todos"></Todos>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import Checkbox from "./components/Checkbox.vue";
+
+import Todos from "./components/Todos.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Todos,
+  },
+  props: ["Todos"],
+  data: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          name: "Workout",
+          description: "Workout to stay healthy",
+        },
+        {
+          id: 2,
+          name: "Lawn Mowing",
+          description: "To keep yard Clean",
+        },
+        {
+          id: 3,
+          name: "Grocery",
+          description: "Eat Healthy",
+        },
+      ],
+      styleObject: {
+        color: "gray",
+      },
+    };
+  },
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+}
+.todos {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="todo-Item">
+      <div class="checkbox">
+        <input type="checkbox" />
+      </div>
+
+      <div class="checkboxlabel">
+        <label for="">
+          <h3>{{ todo.description }}</h3>
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+};
+</script>
+
+<style>
+.todo-Item {
+  display: flex;
+}
+</style>
